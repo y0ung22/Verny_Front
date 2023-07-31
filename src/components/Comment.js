b1fbd11f9396b8e1a827f58be9ace86328f2078c
y0ung22 <amy6484@ewhain.net>
2023-07-31 21:13:28 +0900

    merge: 컴포넌트 머지

---
+++ src/components/Comment.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { styled } from "styled-components";
+
+import comment from "../assets/icons/comment.svg";
+import bookmark from "../assets/icons/bookmark.svg";
+
+const ButtonBar = () => {
+  return (
+    <Wrapper>
+      <Btn>
+        <img src={comment} />
+        <span>{}</span>
+      </Btn>
+      <Btn>
+        <img src={bookmark} />
+        <span>{}</span>
+      </Btn>
+    </Wrapper>
+  );
+};
+
+export default ButtonBar;
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 48px;
+  padding: 0px 32px;
+  align-items: center;
+  gap: 24px;
+  flex-shrink: 0;
+  background: rgba(237, 241, 249, 0.8);
+  backdrop-filter: blur(2px);
+`;
+
+const Btn = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  img {
+    width: 12px;
+    height: 12px;
+    flex-shrink: 0;
+  }
+  span {
+    color: var(--n-neutral-10, #1a1c1e);
+    font-family: Pretendard;
+    font-size: 0.75rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 140%;
+  }
+`;
