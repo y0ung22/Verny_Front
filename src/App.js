701af27364e039921c09ba3e070c8d0a87971057
y0ung22 <amy6484@ewhain.net>
2023-07-31 21:49:02 +0900

    feat: App.js 링크 연결

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import ArtPage from "./pages/ArtPage";
+import ArtDetailPage from "./pages/ArtDetailPage";
+import CommentPage from "./pages/CommentPage";
+import PlacePage from "./pages/PlacePage";
+import PlaceDetailPage from "./pages/PlaceDetailPage";
+import MyPage from "./pages/MyPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" component={StartPage} />
-        <Route path="/signup" component={SignupPage} />
-        <Route path="/login" component={LoginPage} />
+        {/*로그인 및 회원가입*/}
+        <Route exact path="/" element={<StartPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/*메인 화면*/}
+        <Route path="/art" element={<ArtPage />} />
+        <Route path="/place" element={<PlacePage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        {/*세부 화면*/}
+        <Route path="/art/detail" element={<ArtDetailPage />} />
+        <Route path="/art/detail/comment" element={<CommentPage />} />
+        <Route path="/place/detail" element={<PlaceDetailPage />} />
+        <Route path="/mypage/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
