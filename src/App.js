2646a6b8f10d0cf230b0d5f347d4dfb29262458e
kyubinjin <2376282@ewhain.net>
2023-07-31 21:50:57 +0900

    feat: 로그인페이지 수정

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import ArtPage from "./pages/ArtPage";
+import ArtDetailPage from "./pages/ArtDetailPage";
+import PlacePage from "./pages/PlacePage";
+import PlaceDetailPage from "./pages/PlaceDetailPage";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" component={StartPage} />
-        <Route path="/signup" component={SignupPage} />
-        <Route path="/login" component={LoginPage} />
+        {/*로그인 및 회원가입*/}
+        <Route exact path="/" element={<StartPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/*메인 화면*/}
+        <Route path="/main" element={<ArtPage />} />
+        <Route path="/place" element={<PlacePage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        {/*세부 화면*/}
       </Routes>
     </Router>
   );
